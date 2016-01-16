04392f22d1332016e54e1b54ca4409788e66afc8
yousui115 <yousui115@gmail.com>
2016-01-16 12:41:27 +0900

    v2.0 Commit!
    v2.0 リリース用コミット

--- src/main/java/yousui115/db/DB.java
+++ src/main/java/yousui115/db/DB.java
@@ -30,7 +30,7 @@ public class DB
 {
     public static final String MOD_ID = "db";
     public static final String MOD_DOMAIN = "yousui115." + MOD_ID;
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "v2.0";
 
   //■このクラスのインスタンス
     @Mod.Instance(DB.MOD_ID)
